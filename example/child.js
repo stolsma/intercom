2601df963e3a9e7450f8f0356cd7e160f89c9c2d
Sander Tolsma <github@tolsma.net>
2011-10-16 22:58:21 +0200

    [medium] Make intercom startup invisible in the child process

--- example/child.js
+++ example/child.js
@@ -8,18 +8,16 @@
  * @docauthor TTC/Sander Tolsma
  */
 
-require('../lib/intercom');
-
 process.parent.ready(function() {
-	console.log('Comms Ready, sending message!');
-	process.parent.emit('child::message', 'I am alive!');
+  console.log('Comms Ready, sending message!');
+  process.parent.emit('child::message', 'I am alive!');
 });
 
 process.parent.on('parent::message', function(text) {
-	console.log('The parent says: ', text);
-	process.nextTick(function() {
-		process.parent.emit('child::quit');
-	});
+  console.log('The parent says: ', text);
+  process.nextTick(function() {
+    process.parent.emit('child::quit');
+  });
 });
 
 console.log('Child is setup!!');
--- example/intercom.js
+++ example/intercom.js
@@ -9,28 +9,28 @@
  */
 
 var path = require('path'),
-	Child = require('../lib/intercom').EventChild;
+    Child = require('../lib/intercom').EventChild;
 
 var child = Child(path.join(__dirname, 'child.js'));
 
 child.on('stdout', function(txt) {
-	console.log('child stdout: ' + txt);
+  console.log('child stdout: ' + txt);
 });
 
 child.on('stderr', function(txt) {
-	console.log('child stderr: ' + txt);
+  console.log('child stderr: ' + txt);
 });
 
 child.on('child::message', function(text) {
-	console.log('Parent: Child says: ', text);
-	child.emit('parent::message', 'This is your parent!');
+  console.log('Parent: Child says: ', text);
+  child.emit('parent::message', 'This is your parent!');
 });
 
 child.on('child::quit', function() {
-	console.log('Parent: Child wants to quit!');
-	process.nextTick(function(){
-		child.stop();
-	});
+  console.log('Parent: Child wants to quit!');
+  process.nextTick(function(){
+    child.stop();
+  });
 });
 
 child.start();
